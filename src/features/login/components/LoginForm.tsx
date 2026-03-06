8783d047acf554731e89392469a6b5f0084a9ae2
Mora <moram30fercholin@gmail.com>
2026-03-05 18:37:04 -0600

    vista de cocina y arreglo de sus componenetes

--- src/features/login/components/LoginForm.tsx
+++ src/features/login/components/LoginForm.tsx
@@ -93,7 +93,7 @@ export default function LoginForm() {
       </div>
 
       <button className="w-full flex items-center justify-center gap-3 py-3 border rounded-lg">
-        <img src="/google-icon.svg" alt="Google" className="w-5 h-5" />
+        <Image src="/google-icon.svg" alt="Google" width={20} height={20} />
         <span className="font-semibold">Google</span>
       </button>
 
--- src/features/register/components/RegisterForm.tsx
+++ src/features/register/components/RegisterForm.tsx
@@ -106,7 +106,7 @@ export default function RegisterForm() {
       </div>
 
       <button className="w-full flex items-center justify-center gap-3 py-3 border rounded-lg">
-        <img src="/google-icon.svg" alt="Google" className="w-5 h-5" />
+        <Image src="/google-icon.svg" alt="Google" width={20} height={20} />
         <span className="font-semibold">Google</span>
       </button>
 
