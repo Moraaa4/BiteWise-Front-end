43edd249b94eed523a2488fc3176ee1e83efee33
Mora <moram30fercholin@gmail.com>
2026-03-12 10:16:26 -0600

    cambio en landignpage

--- src/app/landing/page.tsx
+++ src/app/landing/page.tsx
@@ -168,7 +168,7 @@ export default function LandingPage() {
             }}
           >
             <h2 className="text-4xl lg:text-6xl font-black leading-tight">
-              <span className="text-emerald-500">60%</span> del desperdicio de comida ocurre en casa*
+              <span className="text-emerald-500">60%</span> del desperdicio de comida ocurre en casa
             </h2>
             <p className="text-xs font-bold text-zinc-400 uppercase tracking-[0.2em]">
               *Food Waste Index Report 2024 UNEP
